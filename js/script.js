077250c6d7dbbc8d3eca1a3b61ed88baa1f05199
Ralitsa Voronevska <ralicavoronevska@gmail.com>
2020-05-30 02:02:45 +0300

    Updates

--- js/script.js
+++ js/script.js
@@ -1,51 +1,69 @@
-
 // Cards Filters
-(function (){
-  "use strict";
-  
-  let btnAll = document.getElementById("all"),
-      btnFound = document.getElementById("found"),
-      btnLost = document.getElementById("lost"),
-      foundCards = document.querySelectorAll(".found"),
-      lostCards = document.querySelectorAll(".lost"),
-      allCards = document.querySelectorAll(".card__item");
- 
+(function () {
+  'use strict';
+
+  let scroll = document.querySelector('.scrollTop');
+  window.addEventListener('scroll', function() {
+    scroll.classList.toggle("active", window.scrollY > 500);
+  });
+  scroll.onclick = function() { window.scrollTo({ top: 0, behavior: 'smooth' }); }
+
+  let btnAll = document.getElementById('all'),
+    btnFound = document.getElementById('found'),
+    btnLost = document.getElementById('lost'),
+    foundCards = document.querySelectorAll('.found'),
+    lostCards = document.querySelectorAll('.lost'),
+    allCards = document.querySelectorAll('.card__item');
+
   // Filter All
-  btnAll.onclick = function() {
-    btnFound.classList.remove("active--btn");
-    btnLost.classList.remove("active--btn");
+  btnAll.onclick = function () {
+    btnFound.classList.remove('active--btn');
+    btnLost.classList.remove('active--btn');
     btnAll.classList.add('active--btn');
-    for (let cards of allCards) { cards.style.display = "block"; }
-  }
+    for (let cards of allCards) {
+      cards.style.display = 'block';
+    }
+  };
 
   // Filter Found
-  btnFound.onclick = function() {
-    btnAll.classList.remove("active--btn");
-    btnLost.classList.remove("active--btn");
-    btnFound.classList.add("active--btn");
-    for (let cards of lostCards) { cards.style.display = "none"; }
-    for (let cards of foundCards) { cards.style.display = "block"; }
+  btnFound.onclick = function () {
+    btnAll.classList.remove('active--btn');
+    btnLost.classList.remove('active--btn');
+    btnFound.classList.add('active--btn');
+    for (let cards of lostCards) {
+      cards.style.display = 'none';
+    }
+    for (let cards of foundCards) {
+      cards.style.display = 'block';
+    }
   };
 
   // Filter Lost
-  btnLost.onclick = function() {
-    btnAll.classList.remove("active--btn");
-    btnFound.classList.remove("active--btn");
+  btnLost.onclick = function () {
+    btnAll.classList.remove('active--btn');
+    btnFound.classList.remove('active--btn');
     btnLost.classList.add('active--btn');
-    for (let cards of foundCards) { cards.style.display = "none"; }
-    for (let cards of lostCards) { cards.style.display = "block"; }
+    for (let cards of foundCards) {
+      cards.style.display = 'none';
+    }
+    for (let cards of lostCards) {
+      cards.style.display = 'block';
+    }
   };
 
   // tabCards & Blog Posts Show More Functionality
-  document.querySelectorAll(".show-more").forEach(function (p) {
-    p.querySelector("button").addEventListener("click", function () {
-      p.classList.toggle("show");
+  document.querySelectorAll('.show-more').forEach(function (p) {
+    p.querySelector('button').addEventListener('click', function () {
+      p.classList.toggle('show');
       if (p.classList.contains('card__desc')) {
-        this.textContent = p.classList.contains("show") ? "по-малко детайли" : "повече детайли";
+        this.textContent = p.classList.contains('show')
+          ? 'по-малко детайли'
+          : 'повече детайли';
       } else {
-        this.textContent = p.classList.contains("show") ? "Прочети по-малко" : "Прочети повече";
+        this.textContent = p.classList.contains('show')
+          ? 'Прочети по-малко'
+          : 'Прочети повече';
       }
     });
   });
-
-}());
+})();
